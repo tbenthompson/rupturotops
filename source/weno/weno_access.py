969a8d78c6ff6a734353db3cff626fc2ecf52a1e
Ben Thompson <t.ben.thompson@gmail.com>
2013-11-09 03:08:37 -0500

    BROKEN COMMIT: Working on nonuniform pyweno.

--- source/weno/weno_access.py
+++ source/weno/weno_access.py
@@ -5,10 +5,118 @@ Original code written by Matthew Emmett as part of PyWENO.
 Edited by Ben Thompson.
 """
 import numpy as np
-import pickle
-from weno_code_gen import jiang_shu_file
+from core.debug import _DEBUG
+import weno_code_gen as wcg
 
-def jiang_shu_smoothness_coefficients(k, x, filename=None):
+
+def call_coeffs(k, d, j, args)
+
+
+def reconstruction_coefficients(k, xi, x, d=0):
+    r"""Numerically compute the reconstruction coefficients for a 2k-1
+    order WENO scheme corresponding to the reconstruction points in *xi*
+    on the non-uniform grid *x*.
+
+    The reconstruction points in *xi* should be in :math:`[-1, 1]`.  This
+    interval is then mapped to the cell :math:`[x_{i-1/2}, x_{i+1/2}]`.
+
+    :param xi: list of reconstruction points
+    :param k: reconstruction order
+    :param x: cell boundaries
+
+    The returned coefficients are stored in a NumPy array that is
+    indexed according to ``c[i,l,r,j]``.  That is
+
+    .. math::
+
+        f^r(\xi_l) \approx \sum_j c^{l,r}_{i,j} \, f_{i-r+j}
+
+    for each :math:`l` from 0 to ``len(xi)``.
+
+    """
+    X = np.array(x)
+    N = len(X) - 1
+    n = len(xi)
+
+    c = np.zeros((N, n, k, k))       # indexed as c[i,l,r,j]
+
+    for i in range(k - 1, N - k + 1):
+        for l in range(n):
+            # Map the point under consideration back into original
+            # spatial domain
+            z = 0.5 * (X[i] + X[i + 1]) +\
+                0.5 * (X[i + 1] - X[i]) * xi[l]
+            for r in range(k):
+                args = []
+                args
+                fnc =
+                c[i, l, r, :] = rc.reconstruction_coeffs(z, i, r, k, X)
+
+
+def optimal_weights(k, xi, x, tolerance=1e-12):
+    r"""Compute the optimal weights for a 2k-1 order WENO scheme
+    corresponding to the reconstruction points in *xi* on the
+    non-uniform grid *x*.
+
+    The coefficients are stored in a NumPy array that is indexed
+    according to ``w[i,l,r]``.  That is
+
+    .. math::
+
+        f(\xi^l) \approx \sum_r w^r f^r
+
+    for each :math:`l` from 0 to ``len(xi)``.
+    """
+
+    X = np.array(x)
+    n = len(xi)
+    N = len(X) - 1
+
+    c = reconstruction_coefficients(k, xi, X)
+
+    # add on a periodic extension...
+    X2 = np.zeros(N + 2 * (k) + 1, X.dtype)
+    X2[k:-k] = X
+    X2[:k] = X[0] - (X[-1] - X[-k - 1:-1])
+    X2[-k:] = X[-1] + (X[1:k + 1] - X[0])
+    c2k = reconstruction_coefficients(2 * k - 1, xi, X2)
+
+    # chop off the extra bits
+    c2k = c2k[k:-k, ...]
+
+    varpi = np.zeros((N, n, k))
+    for i in range(k - 1, N - k + 1):
+        for l in range(n):
+
+            # we'll just use the first k eqns since the system is
+            # overdetermined
+            omega = {}
+            for j in range(k):
+
+                rmin = max(0, (k - 1) - j)
+                rmax = min(k - 1, 2 * (k - 1) - j)
+
+                accum = 0.0
+                for r in range(rmin + 1, rmax + 1):
+                    accum = accum + omega[r] * c[i, l, r, r - (k - 1) + j]
+
+                omega[rmin] = (c2k[i, l, k - 1, j] - accum) / c[
+                    i, l, rmin, rmin - (k - 1) + j]
+
+            # now check all 2*k-1 eqns to make sure the weights
+            # work out properly
+            for r in range(k):
+                varpi[i, l, r] = omega[r]
+
+    return varpi
+
+
+def call_smoothness(module, k, r, i, j, args):
+    fnc_name = wcg.smoothness_fnc_name(k, r, i, j)
+    return wenok.__dict__[fnc_name](*args)
+
+
+def jiang_shu_smoothness_coefficients(k, x):
     r"""Compute the Jiang-Shu smoothness coefficients for a 2k-1 order
     WENO scheme on the non-uniform grid *x*.
 
@@ -24,10 +132,6 @@ def jiang_shu_smoothness_coefficients(k, x, filename=None):
     X = np.array(x)
     N = len(X) - 1
 
-    filename = jiang_shu_file(k, 'smoothness', filename)
-    with open(filename, 'r') as f:
-        fncs = pickle.load(f)
-
     # compute reconstruction coefficients for each left shift r
     beta = np.zeros((N, k, k, k))
     for i in range(k - 1, N - k + 1):
@@ -40,14 +144,16 @@ def jiang_shu_smoothness_coefficients(k, x, filename=None):
             # pick out coefficients
             for m in range(k):
                 for n in range(m, k):
-                    c = fncs[r][m][n](*args)
+                    c = call_smoothness(wenok, k, r, m, n, args)
                     if c is not None:
                         beta[i, r, m, n] = float(c)
     return beta
 
+wenok = __import__('weno_3')
+
 
 def test_jiang_shu_smoothness_coefficients():
-    correct = \
+    correct = np.array(
         [[[3.33333333, -10.33333333,   3.66666667],
          [0.,   8.33333333,  -6.33333333],
          [0.,   0.,   1.33333333]],
@@ -56,12 +162,12 @@ def test_jiang_shu_smoothness_coefficients():
           [0.,   0.,   1.33333333]],
          [[1.33333333,  -6.33333333,   3.66666667],
           [0.,   8.33333333, -10.33333333],
-          [0.,   0.,   3.33333333]]]
+          [0.,   0.,   3.33333333]]])
     # Check for the correct answer on a uniform grid.
     beta = jiang_shu_smoothness_coefficients(3, [1, 2, 3, 4, 5, 6, 7])
-    assert(np.testing.assert_almost_equal(beta[3], correct))
+    np.testing.assert_almost_equal(beta[3], correct)
 
     # Make sure it doesn't fail in the case of a non-uniform grid.
     # How to check correctness?
-    beta2 = jiang_shu_smoothness_coefficients(3, [1, 2, 3, 4, 5, 6, 7])
+    beta2 = jiang_shu_smoothness_coefficients(3, [1, 2, 3, 3.5, 5, 6, 7])
     assert(beta2 is not None)
--- source/weno/weno_code_gen.py
+++ source/weno/weno_code_gen.py
@@ -5,32 +5,13 @@ Original code written by Matthew Emmett as part of PyWENO.
 Edited by Ben Thompson.
 """
 
-
+import subprocess
 import numpy as np
+import numpy.f2py as f2py
 import sympy
-# from sympy.utilities.codegen import codegen
+from sympy.utilities.codegen import codegen
 
 from core.debug import _DEBUG
-
-#
-def module_name(k, type, f=None):
-    """
-    Returns the proper module name for WENO
-    coefficients.
-
-    The modules are created as [f]_[type]_[k]
-
-    :param type: "smoothness", "weights", or "coefficients"
-    :param k: reconstruction order
-    :param f: base prefix for filenames
-    """
-    if f is None:
-        f = 'weno'
-    name = f + '_' + type + '_' + str(k)
-    return name
-
-def test_module_name():
-    assert(module_name(3, 'abcdef') == 'weno_abcdef_3')
 
 def polynomial_interpolator(x, y):
     """Build a symbolic polynomial that interpolates the points (x_i, y_i).
@@ -93,7 +74,7 @@ def test_prim_polynomial_interpolator():
 
 def _pt(a, b, x):
     """Map x in [-1, 1] to x in [a, b]."""
-    half = sympy.sympify(1/2)
+    half = sympy.sympify(1.0/2.0)
     w = half * (b - a)
     c = half * (a + b)
 
@@ -109,7 +90,7 @@ def test_pt():
     assert(_pt(a, 0, x) == 0.5 * (-a * x + a))
 
 
-def coeffs_fnc_name(k, d, r, j):
+def coeffs_fnc_name(k, d, j):
     return 'reconstruction_coeffs_' + str(k) + '_' + str(d) + '_' + str(j)
 
 def coeffs(k, d):
@@ -118,122 +99,67 @@ def coeffs(k, d):
     fs = [sympy.var('f%d' % j) for j in range(k)]
 
     x = sympy.var('x')
-    z = _pt(xs[k - 1], xs[k], x)
+    # z = _pt(xs[k - 1], xs[k], x)
 
     # compute reconstruction coefficients for each left shift r
     fnc_list = []
     poly = primitive_polynomial_interpolator(xs, fs).diff(x, d + 1)
-    _DEBUG(k)
-    poly = poly.simplify().expand()
     for j in range(k):
-        args = xs
-        args.extend(fs)
-        args.append(z)
-        fnc = poly.coeff(fs[j]).simplify()
-        fnc_name = coeffs_fnc_name('reconstruction_coeffs', k, d, j)
+        args = xs[:]
+        args.append(x)
+        fnc = sympy.Poly(poly, fs[j]).coeff_monomial(fs[j]).simplify()
+        fnc_name = coeffs_fnc_name(k, d, j)
         fnc_list.append((fnc, fnc_name, args))
     return fnc_list
 
 def test_reconstruction_coeffs_gen():
     result = coeffs(3, 0)
+    assert(len(result) == 3)
+    assert(result[0][0] != 0)
+    assert(result[1][2].count(sympy.var('x1')) == 1)
+    result2 = coeffs(3, 2)
+    assert(result2)
     pass #how to test code generation? probably just wait until we get the results
 
 
-def optimal_weights(k, xi, x, tolerance=1e-12):
-    r"""Compute the optimal weights for a 2k-1 order WENO scheme
-    corresponding to the reconstruction points in *xi* on the
-    non-uniform grid *x*.
-
-    The coefficients are stored in a NumPy array that is indexed
-    according to ``w[i,l,r]``.  That is
-
-    .. math::
-
-        f(\xi^l) \approx \sum_r w^r f^r
-
-    for each :math:`l` from 0 to ``len(xi)``.
+def smoothness_fnc_name(k, r, i, j):
     """
-
-    X = np.array(x)
-    n = len(xi)
-    N = len(X) - 1
-
-    c = reconstruction_coefficients(k, xi, X)
-
-    # add on a periodic extension...
-    X2 = np.zeros(N + 2 * (k) + 1, X.dtype)
-    X2[k:-k] = X
-    X2[:k] = X[0] - (X[-1] - X[-k - 1:-1])
-    X2[-k:] = X[-1] + (X[1:k + 1] - X[0])
-    c2k = reconstruction_coefficients(2 * k - 1, xi, X2)
-
-    # chop off the extra bits
-    c2k = c2k[k:-k, ...]
-
-    varpi = np.zeros((N, n, k))
-    for i in range(k - 1, N - k + 1):
-        for l in range(n):
-
-            # we'll just use the first k eqns since the system is
-            # overdetermined
-            omega = {}
-            for j in range(k):
-
-                rmin = max(0, (k - 1) - j)
-                rmax = min(k - 1, 2 * (k - 1) - j)
-
-                accum = 0.0
-                for r in range(rmin + 1, rmax + 1):
-                    accum = accum + omega[r] * c[i, l, r, r - (k - 1) + j]
-
-                omega[rmin] = (c2k[i, l, k - 1, j] - accum) / c[
-                    i, l, rmin, rmin - (k - 1) + j]
-
-            # now check all 2*k-1 eqns to make sure the weights work out properly
-            # XXX
-
-            for r in range(k):
-                varpi[i, l, r] = omega[r]
-
-    return varpi
-
-
-
-
-def jiang_shu_smoothness_create(k, filename=None):
+    Arguments:
+        :param k: The order of the polynomial reconstruction.
+        :param i: The first dimension of the smoothness kernel position.
+        :param j: The second dimension of the smoothness kernel position.
     """
-    Compute the Jiang-Shu smoothness coefficient functions and store them in
-    specified file.
+    return 'smoothness_' + str(k) + '_' + str(r) + '_' + str(i) + '_' + str(j)
 
-    This function uses the sympy autowrap module to create fortran functions
-    using f2py. It is massively faster at runtime than directly calling the sympy functions
-    using subs. As a tradeoff, this takes a couple minutes to generate the fortran
-    functions.
+
+def smoothness(k):
+    """
+    Compute the Jiang-Shu smoothness coefficient functions and output them in a form ready
+    to be compiled to fortran.
     """
 
     # the integration variable (represents position within the cell)
     x = sympy.var('x')
 
-    # build array of cell boundaries (sympy vars x[i])
+    # build array of cell boundaries and cell averages (sympy vars x[i])
     xs = []
     for j in range(k + 1):
         xs.append(sympy.var('x%d' % j))
+    # and build the array of cell averages
+    ys = []
+    for j in range(k):
+        ys.append(sympy.var('y%d' % j))
 
     # The upper and lower boundaries of the cell, used as the integration
     # bounds.
     b1 = sympy.var('b1')
     b2 = sympy.var('b2')
 
-    # and build the array of cell averages
-    ys = []
-    for j in range(k):
-        ys.append(sympy.var('y%d' % j))
-
     p = primitive_polynomial_interpolator(xs, ys)
     p = p.as_poly(x)
     p = p.diff(x)
 
-    ppp = {}
+    ppp = []
     for r in range(0, k):
         # sum of L^2 norms of derivatives
         s = 0
@@ -243,43 +169,56 @@ def jiang_shu_smoothness_create(k, filename=None):
             pp = (b2 - b1) ** (2 * j - 1) * (
                 pp.subs(x, b2) - pp.subs(x, b1))
             s = s + pp
+        ppp.append(s.simplify().expand())
 
-        # This is slow but, because of some bug in sympy, the 'coeff'
-        # function doesn't work properly otherwise?!
-        ppp[r] = s.simplify().expand()
-
-    # Take the smoothness expressions and make a fortran function out
-    # of each coefficient. Pickle these functions to the specified file
-    filename = jiang_shu_file(k, filename)
+    # Return functions and arguments in a form ready to be compiled to fortran
+    # expressions that can be called from the live code.
     fncs = []
     for r in range(0, k):
-        coeffs_list = []
         for i in range(0, k):
-            inner_coeffs_list = []
             for j in range(0, k):
                 args = [xs[m] for m in range(0, k + 1)]
+                # I suspect that I could carefully redesign this so that
+                # b1 and b2 would be drawn from the xs array
                 args.append(b1)
                 args.append(b2)
-                coeffs_expr = ppp[r].coeff(ys[i] * ys[j])
-                _DEBUG()
-                # to finish this implementation, i would need to use the sympy codegen utility and
-                # f2py to create fortran functions for the smoothness coefficients. write the
-                # code here
-                # l = autowrap([coeffs_expr], args=args,
-                # tempdir=autogen_fortran/
-                inner_coeffs_list.append(l)
-            coeffs_list.append(inner_coeffs_list)
-        fncs.append(coeffs_list)
-    _DEBUG(2)
-
-
-# def generate_function(coeffs_expr, args, directory, name):
-
-# This test takes forever to run, so it's okay to turn it off unless it's
-# needed.
+                fnc = ppp[r].coeff(ys[i] * ys[j]).simplify()
+                fnc_name = smoothness_fnc_name(k, r, i, j)
+                fncs.append((fnc, fnc_name, args))
+    return fncs
 
 
 def test_jiang_shu_smoothness_create():
-    jiang_shu_smoothness_create(3)
+    assert(smoothness(3))
 
 
+def create_fncs(k):
+    """
+    This function uses the sympy autowrap module to create fortran functions
+    with f2py. It is massively faster at runtime than directly calling the
+    sympy functions using subs. As a tradeoff, this takes a couple minutes
+    to generate the fortran functions.
+    """
+    filename_prefix = 'weno_' + str(k)
+    fncs = []
+    for d in range(0, 1):
+        fncs.extend(coeffs(k, d))
+    fncs.extend(smoothness(k))
+    args_list = [f[2] for f in fncs]
+    fncs_and_names = [(f[1], f[0]) for f in fncs]
+    source_file = filename_prefix + '.f90'
+    with open(source_file, 'w') as ff:
+        ff.write('! -*- f90 -*-\n')
+        for i in range(len(fncs_and_names)):
+            code = codegen(fncs_and_names[i], "F95", "autoweno",
+                            argument_sequence=args_list[i])
+            ff.write(code[0][1])
+    subprocess.call('f2py -c ' + source_file + ' -m ' + filename_prefix,
+                    shell=True)
+    subprocess.call('rm ' + source_file, shell=True)
+    module = __import__(filename_prefix)
+    assert(module)
+
+
+def test_create_fncs():
+    create_fncs(3)
